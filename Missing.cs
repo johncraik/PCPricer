e3207576b050d5b177396d9df00764f87316fbca
johncraik <jgcraik1@sheffield.ac.uk>
2022-04-14 21:11:15 +0100

    Added missing and pricechg classes [#36]

---
+++ Missing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCPricer
+{
+    internal class Missing : Part
+    {
+        public Missing(string brand, string model, float price) : base(brand, model, price){ }
+    }
+}
---
+++ PriceChg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCPricer
+{
+    internal class PriceChg : Part
+    {
+
+
+        public PriceChg(string brand, string model, float price) : base(brand, model, price)
+        {
+
+        }
+    }
+}
