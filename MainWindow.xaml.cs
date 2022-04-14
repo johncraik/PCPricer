83ed2efb9094a1c1bd5e4771976f780c716d395c
johncraik <jgcraik1@sheffield.ac.uk>
2022-04-14 12:37:49 +0100

    Created new static class for static functions [#23]

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,38 +27,10 @@ namespace PCPricer
             InitializeComponent();
         }
 
-        private static void RunPython(string script)
-        {
-            //Obtain the relative path of the program:
-            string path = Environment.CurrentDirectory;
-            path = path[..path.IndexOf("PCPricer")] + @"\PCPricer\scripts";
-
-            //Start a new process to run cmd
-            Process p = new();
-            ProcessStartInfo pInfo = new()
-            {
-                FileName = "cmd.exe",           //File to open is cmd
-                RedirectStandardInput = true,   //Redirect input to the program (rather than user input)
-                UseShellExecute = false     
-            };
-
-            //Strat the process:
-            p.StartInfo = pInfo;
-            p.Start();
-
-            //Write to process as standard input (run commands in cmd):
-            using StreamWriter sw = p.StandardInput;
-            if (sw.BaseStream.CanWrite)
-            {
-                sw.WriteLine("cd " + path);         //Move to correct file location in cmd
-                sw.WriteLine("python " + script);   //Run python script from cmd
-            }
-        }
-
         private void Btn_Refresh_Click(object sender, RoutedEventArgs e)
         {
             //Run python script 'ppp.py' (ppp = pcpartpicker)
-            RunPython("ppp.py");
+            Resfile.RunPython("ppp.py");
         }
 
         private void Btn_OpenSCR_Click(object sender, RoutedEventArgs e)
---
+++ Resfile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCPricer
+{
+    internal static class  Resfile
+    {
+        public static void RunPython(string script)
+        {
+            //Obtain the relative path of the program:
+            string path = Environment.CurrentDirectory;
+            path = path[..path.IndexOf("PCPricer")] + @"\PCPricer\scripts";
+
+            //Start a new process to run cmd
+            Process p = new();
+            ProcessStartInfo pInfo = new()
+            {
+                FileName = "cmd.exe",           //File to open is cmd
+                RedirectStandardInput = true,   //Redirect input to the program (rather than user input)
+                UseShellExecute = false
+            };
+
+            //Strat the process:
+            p.StartInfo = pInfo;
+            p.Start();
+
+            //Write to process as standard input (run commands in cmd):
+            using StreamWriter sw = p.StandardInput;
+            if (sw.BaseStream.CanWrite)
+            {
+                sw.WriteLine("cd " + path);         //Move to correct file location in cmd
+                sw.WriteLine("python " + script);   //Run python script from cmd
+            }
+        }
+    }
+}
